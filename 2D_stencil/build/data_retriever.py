4edf5bea98d98dcd9bf6a77b4bff38c30b889c74
Bjørnar Veie <bjornarveie@Bjrnars-MacBook-Air.local>
2019-05-26 14:53:16 +0200

    should work

--- 2D_stencil/build/data_retriever.py
+++ 2D_stencil/build/data_retriever.py
@@ -3,5 +3,15 @@ import pandas as pd
 df_run_openmp = pd.read_csv('run_data_openmp.txt',sep = "\t",header=None,names=['Nx','Ny','Threads','Steps'])
 
 df_results_openmp = pd.read_csv('results_openmp.txt',sep = "\t",header=None,names=['MLUPS','Time'])
+
+
+df_results_parallel = pd.read_csv('resultstxt',sep = "\t",header=None,names=['MLUPS','Time'])
+
+df_run_parallel= pd.read_csv('run_data.txt',sep = "\t",header=None,names=['Nx','Ny','Threads','Steps','Localities','Local partitions'])
 print(df_run_openmp)
 print(df_results_openmp)
+
+print('---------------------')
+
+print(df_run_parallel)
+print(df_results_parallel)
